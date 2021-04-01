6399939f8c3d5495a77c1b248480a571c37617ef
woorame <kwrm90@themyplanet.com>
2021-04-01 18:07:32 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,14 @@ function login() {
     body: JSON.stringify(req)
   })
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      if (res.success) {
+        location.href = '/';
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error('로그인 중 에러 발생'));
+    });
 }
