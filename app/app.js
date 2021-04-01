0ea8a8a4120841d975132354c77e8e248bc529a4
woorame <kwrm90@themyplanet.com>
2021-04-01 16:12:06 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 // 라우팅
-const home = require('./routes/home'); // 해당 경로에 있는 js 파일을 읽어옴
+const home = require('./src/routes/home'); // 해당 경로에 있는 js 파일을 읽어옴
 
 // 앱 세팅
-app.set('views', './views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs'); // ejs 엔진을 사용하기 위한 set
 
 // index.js 에서 exports한 router를 app.js에서 받아온다.
